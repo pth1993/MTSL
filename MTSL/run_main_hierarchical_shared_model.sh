8459990de28316cd2189f0c362eea7402bebf8de
pth1993 <phamthaihoang.hn@gmail.com>
2019-03-15 10:24:22 -0400

    add hierarchical_shared_model

--- MTSL/run_main_hierarchical_shared_model.sh
+++ MTSL/run_main_hierarchical_shared_model.sh
@@ -4,7 +4,7 @@ CUDA_VISIBLE_DEVICES=0 python main_hierarchical_shared_model.py --rnn_mode LSTM 
  --gamma 0.0 --p_in 0.33 --p_rnn 0.33 0.5 --p_out 0.5 --bigram --lm_loss 0.05 \
  --embedding_path "data/embedding/glove_embedding.txt" --option_path "data/embedding/elmo_option.json" \
  --weight_path "data/embedding/elmo_weight.hdf5" --word2index_path "output/joint_model_2_layer/chunk/word2index" \
- --out_path "output/joint_model_2_layer/chunk" --use_crf "True" --use_lm "False", --use_elmo "False" \
+ --out_path "output/joint_model_2_layer/chunk" --use_crf "True" --use_lm "True", --use_elmo "True" \
  --label_type "chunk" "fgner" \
  --train "data/chunk/train.chunk" "data/fgner/train.fgner" \
  --dev "data/chunk/dev.chunk" "data/fgner/dev.fgner" \
