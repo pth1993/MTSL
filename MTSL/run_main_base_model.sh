1d3a83cd071375009cb3b781d7fd219f502be87e
pth1993 <phamthaihoang.hn@gmail.com>
2019-03-16 12:28:24 -0400

    edit main script

--- MTSL/run_main_base_model.sh
+++ MTSL/run_main_base_model.sh
@@ -4,7 +4,7 @@ CUDA_VISIBLE_DEVICES=0 python main_base_model.py --rnn_mode LSTM --num_epochs 20
  --gamma 0.0 --p_in 0.33 --p_rnn 0.33 0.5 --p_out 0.5 --bigram --lm_loss 0.05 \
  --embedding_path "data/embedding/glove_embedding.txt" --option_path "data/embedding/elmo_option.json" \
  --weight_path "data/embedding/elmo_weight.hdf5" --word2index_path "output/base_model/word2index" \
- --out_path "output/base_model" --use_crf "False" --use_lm "False" --use_elmo "False" --label_type "ner" \
+ --out_path "output/base_model" --use_crf "True" --use_lm "True" --use_elmo "True" --label_type "ner" \
  --bucket 5 10 15 20 25 30 40 50 60 70 80 \
  --train "data/ner/train.ner" \
  --dev "data/ner/dev.ner" \
